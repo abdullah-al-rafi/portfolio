fbae9753e5d77267424b5460ffb4ef7b2fa1bd8e
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2020-09-02 03:43:40 +0600

    back to top button added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import "./App.css";
 import ContactForm from "./components/contactForm/ContactForm";
 import Slider from "./components/carousel/Slider";
 import Footer from "./components/footer/Footer";
+import Top from "./components/toTop/Top";
 
 function App() {
   return (
     <div className="App">
       <Slider />
       <ContactForm />
+      <Top />
       <Footer />
     </div>
   );
--- src/components/carousel/Slider.js
+++ src/components/carousel/Slider.js
@@ -6,38 +6,40 @@ import "./slider.css";
 function Slider() {
   return (
     <React.Fragment>
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={require("../img/sea.webp")}
-            alt="Image"
-          />
-          <Carousel.Caption>
-            <p>Scenery 1</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={require("../img/leo.webp")}
-            alt="Image"
-          />
-          <Carousel.Caption>
-            <p>Scenery 2</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={require("../img/nature.webp")}
-            alt="Image"
-          />
-          <Carousel.Caption>
-            <p>Scenery 3</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <div>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={require("../img/sea.webp")}
+              alt="Image"
+            />
+            <Carousel.Caption>
+              <p>Scenery 1</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={require("../img/leo.webp")}
+              alt="Image"
+            />
+            <Carousel.Caption>
+              <p>Scenery 2</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={require("../img/nature.webp")}
+              alt="Image"
+            />
+            <Carousel.Caption>
+              <p>Scenery 3</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </React.Fragment>
   );
 }
