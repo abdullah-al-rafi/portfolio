ac0f1e7094f784fcc6f47c3edef999c9d96e30f0
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2020-09-04 16:42:27 +0600

    icon with links added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
 import "./footer.css";
 
 function Footer() {
@@ -6,8 +7,23 @@ function Footer() {
     <React.Fragment>
       <footer className="page-footer font-small blue">
         <div className="footer-copyright text-center py-3">
-          © 2020 Copyright:
-          <a href="http://alrafi-portfolio.herokuapp.com"> Abdullah Al Rafi</a>
+          <a
+            target="__blank"
+            href="https://www.linkedin.com/in/abdullahalrafi/"
+          >
+            <FaLinkedin size={40} style={{ color: "#005888" }} />
+          </a>{" "}
+          <a target="__blank" href="https://github.com/alrafiabdullah">
+            <FaGithubSquare size={40} style={{ color: "#000" }} />
+          </a>
+          <br />© 2020 Copyright:
+          <a
+            style={{ color: "#0C4B33" }}
+            href="http://alrafi-portfolio.herokuapp.com"
+          >
+            {" "}
+            Abdullah Al Rafi
+          </a>
         </div>
       </footer>
     </React.Fragment>
