5b8c764d31126606453ac94f653569ae6130c5ae
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2020-09-05 03:20:28 +0600

    footer updated

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
+import {
+  FaLinkedin,
+  FaGithubSquare,
+  FaMedium,
+  FaDev,
+  FaInstagramSquare,
+} from "react-icons/fa";
 import "./footer.css";
 
 function Footer() {
@@ -8,26 +14,37 @@ function Footer() {
       <footer className="page-footer font-small blue">
         <div className="footer-copyright text-center py-3">
           <a
-            target="_blank"
-            className="badge"
-            href="http://www.djangoproject.com/"
-          >
-            <img
-              src="https://www.djangoproject.com/m/img/badges/djangomade124x25.gif"
-              border="0"
-              alt="Made with Django & React."
-              title="Made with Django & React."
-            />
-          </a>
-          <a
             target="__blank"
             href="https://www.linkedin.com/in/abdullahalrafi/"
           >
-            <FaLinkedin size={40} style={{ color: "#005888" }} />
+            <FaLinkedin size={40} style={{ color: "#0073B0" }} />
           </a>{" "}
           <a target="___blank" href="https://github.com/alrafiabdullah">
             <FaGithubSquare size={40} style={{ color: "#000" }} />
           </a>{" "}
+          <a target="____blank" href="https://medium.com/@abdullahalrafi">
+            <FaMedium size={40} style={{ color: "#000" }} />
+          </a>{" "}
+          <a target="_____blank" href="https://dev.to/abdullahalrafi">
+            <FaDev size={40} style={{ color: "#000" }} />
+          </a>{" "}
+          <a
+            target="______blank"
+            href="https://www.instagram.com/its_not_good_but_i_still/"
+          >
+            <FaInstagramSquare size={40} style={{ color: "#E1306C" }} />
+          </a>
+          <br />
+          <br />
+          This website is built with{" "}
+          <a target="_blank" href="https://www.djangoproject.com/">
+            Django
+          </a>{" "}
+          &{" "}
+          <a target="_______blank" href="https://reactjs.org/">
+            React
+          </a>
+          .
           <br />© 2020 Copyright:
           <a
             style={{ color: "#0C4B33" }}
