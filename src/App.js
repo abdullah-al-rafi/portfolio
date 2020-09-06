bd2bd97eca5c64a1aaf1f80c3403c5afd54eaff2
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2020-09-07 03:42:55 +0600

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import "./App.css";
+import $ from "jquery";
 
+import PacmanLoader from "react-spinners/PacmanLoader";
 import Body from "./components/body/Body";
-import ContactForm from "./components/contactForm/ContactForm";
 import Typing from "./components/typing/Typing";
 import Footer from "./components/footer/Footer";
 import Top from "./components/toTop/Top";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
+  function loading() {
+    $(window).on("load", () => {
+      $(".loading-screen").fadeOut("slow");
+      document.getElementById("app").style.display = "block";
+    });
+  }
+
+  const Contacts = lazy(() => import("./components/contactForm/ContactForm"));
+
   return (
-    <div className="App">
-      <Typing />
-      <Navbar />
-      <Body />
-      <ContactForm />
-      <Top />
-      <Footer />
-    </div>
+    <React.Fragment>
+      <div onLoadStart={loading()}>
+        <div className="loading-screen">
+          <PacmanLoader size={45} color="#61dafb" />
+        </div>
+        <div className="App" id="app">
+          <Typing />
+          <Navbar />
+          <Body />
+          <Suspense fallback={<div>Loading....</div>}>
+            <Contacts />
+          </Suspense>
+          <Top />
+          <Footer />
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
