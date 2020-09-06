ba48c5b7e248a92f3a5913ff0326083e4a5999a3
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2020-09-07 03:42:03 +0600

    color changed to black and white

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -14,21 +14,35 @@ function Footer() {
       <footer className="page-footer font-small blue">
         <div className="footer-copyright text-center py-3">
           <a
+            rel="noopener noreferrer"
             target="__blank"
             href="https://www.linkedin.com/in/abdullahalrafi/"
           >
             <FaLinkedin size={40} style={{ color: "#0073B0" }} />
           </a>{" "}
-          <a target="___blank" href="https://github.com/alrafiabdullah">
-            <FaGithubSquare size={40} style={{ color: "#000" }} />
-          </a>{" "}
-          <a target="____blank" href="https://medium.com/@abdullahalrafi">
-            <FaMedium size={40} style={{ color: "#000" }} />
-          </a>{" "}
-          <a target="_____blank" href="https://dev.to/abdullahalrafi">
-            <FaDev size={40} style={{ color: "#000" }} />
+          <a
+            rel="noopener noreferrer"
+            target="___blank"
+            href="https://github.com/alrafiabdullah"
+          >
+            <FaGithubSquare size={40} style={{ color: "#fff" }} />
           </a>{" "}
           <a
+            rel="noopener noreferrer"
+            target="____blank"
+            href="https://medium.com/@abdullahalrafi"
+          >
+            <FaMedium size={40} style={{ color: "#fff" }} />
+          </a>{" "}
+          <a
+            rel="noopener noreferrer"
+            target="_____blank"
+            href="https://dev.to/abdullahalrafi"
+          >
+            <FaDev size={40} style={{ color: "#fff" }} />
+          </a>{" "}
+          <a
+            rel="noopener noreferrer"
             target="______blank"
             href="https://www.instagram.com/its_not_good_but_i_still/"
           >
@@ -37,22 +51,34 @@ function Footer() {
           <br />
           <br />
           This website is built with{" "}
-          <a target="_blank" href="https://www.djangoproject.com/">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://www.djangoproject.com/"
+          >
             Django
+          </a>
+          ,{" "}
+          <a
+            rel="noopener noreferrer"
+            target="_______blank"
+            href="https://reactjs.org/"
+            style={{ color: "#61dafb" }}
+          >
+            React
           </a>{" "}
           &{" "}
-          <a target="_______blank" href="https://reactjs.org/">
-            React
+          <a
+            rel="noopener noreferrer"
+            target="________blank"
+            href="https://www.postgresql.org/"
+            style={{ color: "#336690" }}
+          >
+            PostgreSQL
           </a>
           .
           <br />© 2020 Copyright:
-          <a
-            style={{ color: "#0C4B33" }}
-            href="http://alrafi-portfolio.herokuapp.com"
-          >
-            {" "}
-            Abdullah Al Rafi
-          </a>
+          <a href="http://alrafi-portfolio.herokuapp.com"> Abdullah Al Rafi</a>
         </div>
       </footer>
     </React.Fragment>
