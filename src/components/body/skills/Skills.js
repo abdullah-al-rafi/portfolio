313730665cc630a34f19792a3b3650be64cea58b
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2020-09-14 17:01:17 +0600

    skill content added

---
+++ src/components/body/skills/Skills.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Skills() {
+  return (
+    <React.Fragment>
+      <h5>Skills & Interests</h5>
+      <br />
+      <img src="https://img.shields.io/badge/javascript%20-%23323330.svg?&style=for-the-badge&logo=javascript&logoColor=%23F7DF1E" />{" "}
+      <img src="https://img.shields.io/badge/django%20-%23092E20.svg?&style=for-the-badge&logo=django&logoColor=white" />{" "}
+      <img src="https://img.shields.io/badge/html5%20-%23E34F26.svg?&style=for-the-badge&logo=html5&logoColor=white" />{" "}
+      <img src="https://img.shields.io/badge/css3%20-%231572B6.svg?&style=for-the-badge&logo=css3&logoColor=white" />{" "}
+      <img src="https://img.shields.io/badge/bootstrap%20-%23563D7C.svg?&style=for-the-badge&logo=bootstrap&logoColor=white" />{" "}
+      <img src="https://img.shields.io/badge/jquery%20-%230769AD.svg?&style=for-the-badge&logo=jquery&logoColor=white" />{" "}
+      <img src="https://img.shields.io/badge/postgres-%23316192.svg?&style=for-the-badge&logo=postgresql&logoColor=white" />{" "}
+      <img src="https://img.shields.io/badge/git%20-%23F05033.svg?&style=for-the-badge&logo=git&logoColor=white" />{" "}
+      <img src="https://img.shields.io/badge/github%20-%23121011.svg?&style=for-the-badge&logo=github&logoColor=white" />{" "}
+      <img src="https://img.shields.io/badge/react%20-%2320232a.svg?&style=for-the-badge&logo=react&logoColor=%2361DAFB" />{" "}
+      <img src="https://img.shields.io/badge/graphql%20-%2307405e.svg?&style=for-the-badge&logo=graphql&logoColor=white" />{" "}
+      <img src="https://img.shields.io/badge/python-%233776AB.svg?&style=for-the-badge&logo=python&logoColor=white" />
+    </React.Fragment>
+  );
+}
+
+export default Skills;
