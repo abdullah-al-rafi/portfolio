cddfc626c5b774df8c75196a82e1bbfcd7bbed06
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2020-09-14 17:01:30 +0600

    project content added

---
+++ src/components/body/projects/Projects.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <React.Fragment>
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://github.com/alrafiabdullah"
+      >
+        Github Projects!
+      </a>
+    </React.Fragment>
+  );
+}
+
+export default Projects;
